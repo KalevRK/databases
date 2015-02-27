5283fce7bcc3d7f2cba37629ab0779542ded930d
KalevRK <KalevRK@gmail.com>
2015-02-27 15:46:06 -0800

    Set up mySQL database and tables

--- server/schema.sql
+++ server/schema.sql
@@ -1,14 +1,22 @@
-CREATE DATABASE chat;
+CREATE DATABASE IF NOT EXISTS chat;
 
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  ID int NOT NULL AUTO_INCREMENT,
+  Username varchar(30),
+  Message varchar(150),
+  Room varchar(30),
+  PRIMARY KEY (ID)
 );
 
 /* Create other tables and define schemas for them here! */
 
-
+CREATE TABLE users (
+  ID int NOT NULL AUTO_INCREMENT,
+  Username varchar(30),
+  PRIMARY KEY (ID)
+);
 
 
 /*  Execute this file from the command line by typing:
