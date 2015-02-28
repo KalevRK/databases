1c4ea3e31527d8904fe0466086270fbabc1060e0
KalevRK <KalevRK@gmail.com>
2015-02-27 17:30:54 -0800

    Debug issues with posting and getting messages.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -6,9 +6,11 @@ var bluebird = require('bluebird');
 module.exports = {
   messages: {
     get: function (req, res) {
+      console.log('get in controller');
       models.messages.get(req, res);
     }, // a function which handles a get request for all messages
     post: function (req, res) {
+      console.log('post in controller');
       models.messages.post(req, res);
     } // a function which handles posting a message to the database
   },
--- server/models/index.js
+++ server/models/index.js
@@ -9,10 +9,12 @@ module.exports = {
       res.end();
     }, // a function which produces all the messages
     post: function (req, res) {
-      var queryString = 'INSERT INTO messages (Username, Message, Room)
-      VALUES (' + req.body.username + ',' + req.body.text + ',' + req.body.roomname + ')';
+      console.log("we're in models post method, and here's req.body ", req.body)
+      var queryString = 'INSERT INTO messages (Username, Message, Room) ' +
+      'VALUES (' + req.body.username + ',' + req.body.message + ',' + req.body.roomname + ')';
 
-      var query = app.connection.query();
+      var query = app.connection.query(queryString);
+      res.end("All done here");
     } // a function which can be used to insert a message into the database
   },
 
