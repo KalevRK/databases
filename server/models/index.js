b91c60cd187b79a812e7bbf4aea981166c811ef0
KalevRK <KalevRK@gmail.com>
2015-02-27 16:57:03 -0800

    Set up controllers, create connection between model and database

--- server/models/index.js
+++ server/models/index.js
@@ -1,11 +1,13 @@
 var db = require('../db');
-
-
-
+var app = require('../app.js');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
+    get: function (req, res) {
+      var query = app.connection.query('SELECT * FROM messages');
+      res.send(query);
+      res.end();
+    }, // a function which produces all the messages
     post: function () {} // a function which can be used to insert a message into the database
   },
 
