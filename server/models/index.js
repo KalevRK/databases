3cbeb777062319cc6d3fb7cdbffe5bf16cd90046
KalevRK <KalevRK@gmail.com>
2015-02-28 12:28:16 -0800

    Refactor server to use Sequelize

--- server/models/index.js
+++ server/models/index.js
@@ -1,50 +1,69 @@
 var db = require('../db');
 var app = require('../app.js');
+var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize('chat', 'root', '');
+
+var User = sequelize.define('user', {
+  Username: Sequelize.STRING
+});
+
+var Message = sequelize.define('message', {
+  Username: Sequelize.STRING,
+  Message: Sequelize.STRING,
+  Room: Sequelize.STRING
+});
+
+Message.sync().success(function(){
+  console.log("Message.sync() success!!")
+});
+
+User.sync().success(function(){
+  console.log("User.sync() success!!")
+});
 
 module.exports = {
   messages: {
     get: function (req, res) {
-      app.connection.query("SELECT * FROM messages", function(err, results) {
-        if (err) {
-          throw err;
-        }
-        res.send(results);
-
+      var results = Message.findAll().success(function(msg){
+        res.send(msg);
       });
+
+      // app.connection.query("SELECT * FROM messages", function(err, results) {
+      //   if (err) {
+      //     throw err;
+      //   }
+      //   res.send(results);
+
+      // });
 
     }, // a function which produces all the messages
     post: function (req, res) {
       var message = req.body.message.replace("'", "\\'");
-      var queryString = "INSERT INTO messages (Username, Message, Room) " +
-      "VALUES ('" + req.body.username + "','" + message + "','" + req.body.roomname + "')";
-
-      app.connection.query(queryString, function(err, results) {
-        if (err) {
-          throw err;
-        }
-        res.end("All done here");
+      var newMessage = Message.build({
+        Username: req.body.username,
+        Message: message,
+        Room: req.body.roomname
       });
+      newMessage.save().success(function(data){
+        res.send("Great work posting!");
+      })
     } // a function which can be used to insert a message into the database
   },
 
   users: {
     get: function (req, res) {
-      app.connection.query('SELECT * FROM users', function(err, results) {
-        if (err) {
-          throw err;
-        }
-        res.send(results);
+      var results = Users.findAll().success(function(users){
+        res.send(users);
       });
     },
     post: function (req, res) {
-      var queryString = "INSERT INTO users (Username) " +
-      "VALUES ('" + req.body.username + "')";
-      app.connection.query(queryString, function(err, results) {
-        if (err) {
-          throw err;
-        }
-        res.end("All done here");
-      });
+     var newUser = User.build({
+       Username: req.body.username
+     });
+     newUser.save().success(function(data){
+       res.send("Great work posting!");
+     })
     }
   }
 };
